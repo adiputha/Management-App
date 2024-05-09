c1e85d8efff7486a57df56b8cdb21477f7d98d57
adiputha <adithyaperera456@gmail.com>
2024-05-09 11:41:09 +0530

    Bottom sheet creation

--- app/src/main/java/com/example/tasktracker/MainActivity.kt
+++ app/src/main/java/com/example/tasktracker/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.tasktracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.tasktracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener{
+                NewTaskSheet().show(supportFragmentManager,"newTask")
+        }
+
+        taskViewModel.name.observe(this){
+            binding.taskName.text = String.format("Task Name: %s", it)
+        }
+
+        taskViewModel.desc.observe(this){
+            binding.taskDesc.text = String.format("Task Description: %s", it)
+        }
     }
 }
